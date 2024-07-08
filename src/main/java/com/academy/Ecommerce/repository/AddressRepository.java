fb32e34ae9d328907431f31f808fdb9bb6528dcc
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-07-08 11:13:59 +0200

    chore: - add AddressRepository.

---
+++ src/main/java/com/academy/Ecommerce/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.academy.Ecommerce.repository;
+
+import com.academy.Ecommerce.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByUserId(Long userId);
+}
