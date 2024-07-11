bbe35a4fbe936e7d703ff58c2c99343a144a5224
rana.mostafa <rana.mostafa@gap-cloud.com>
2024-07-11 10:10:35 +0300

    security configurations updates

--- src/main/java/com/academy/Ecommerce/config/SecurityConfig.java
+++ src/main/java/com/academy/Ecommerce/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import com.academy.Ecommerce.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
