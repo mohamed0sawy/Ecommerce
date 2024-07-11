97066c12f26584bb8732928bab719682c5f46153
rana.mostafa <rana.mostafa@gap-cloud.com>
2024-07-11 10:09:27 +0300

    updated thymeleaf configurations

--- src/main/java/com/academy/Ecommerce/config/WebMvcConfig.java
+++ src/main/java/com/academy/Ecommerce/config/WebMvcConfig.java
@@ -3,10 +3,9 @@ package com.academy.Ecommerce.config;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -24,6 +23,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setTemplateMode(TemplateMode.HTML);
         return resolver;
     }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.TEXT_HTML)
+                .mediaType("html", MediaType.TEXT_HTML)
+                .mediaType("js", MediaType.valueOf("application/javascript"))
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("application/json", MediaType.valueOf("application/json")); // Add JSON media type
+
+
+    }
 
 
     @Bean
@@ -33,6 +42,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         // Add LayoutDialect for Thymeleaf
         engine.addDialect(new LayoutDialect());
+        engine.addDialect(new SpringSecurityDialect());
+
 
         return engine;
     }
