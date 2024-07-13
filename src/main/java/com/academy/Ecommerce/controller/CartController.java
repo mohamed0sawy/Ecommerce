8c97ebb2443c7c833f62501a21d658c849cb1af8
rana.mostafa <rana.mostafa@gap-cloud.com>
2024-07-13 12:28:54 +0300

    edit cart controller

--- src/main/java/com/academy/Ecommerce/controller/CartController.java
+++ src/main/java/com/academy/Ecommerce/controller/CartController.java
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.*;
 
@@ -56,7 +57,7 @@ public class CartController {
     }
 
     @GetMapping("/add")
-    public String addToCart(@RequestParam("product_id") Long productId, HttpServletRequest request) {
+    public String addToCart(@RequestParam("product_id") Long productId, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         HttpSession session = request.getSession();
         User existingUser = (User) session.getAttribute("user");
         if (existingUser == null) {
@@ -70,6 +71,8 @@ public class CartController {
         } else {
             cartItemService.incrementQuantityByOne(cart, product);
         }
+        redirectAttributes.addFlashAttribute("successMessage", "Item added successfully");
+
         return "redirect:/api/v1/";
     }
 
